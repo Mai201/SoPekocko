e702c267632d541b47466940c46e40f7946b52a0
Mai201 <64534832+Mai201@users.noreply.github.com>
2020-09-03 16:44:32 +0200

    update controllers
    màj pour createSauce et pour getOneSauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -6,7 +6,11 @@ exports.createSauce= (req, res, next) => {
   delete sauceObject._id;
    const sauce = new Sauce({
      ...sauceObject,
-     imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+     imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+     likes:0,
+     dislikes:0,
+     usersLiked:[],
+     usersDisliked:[]
    });
    sauce.save()
      .then(() => res.status(201).json({ message: 'sauce enregistrée !'}))
@@ -20,8 +24,8 @@ exports.getAllSauces=(req, res, next) => {
     };
 
 exports.getOneSauce=(req,res,next) => {
-  Sauce.findOne({_id:req.params.id})
-  .then(thing => res.status(200).json(sauce))
+  Sauce.findOne({ _id:req.params.id })
+  .then(sauce => res.status(200).json(sauce))
   .catch(error => res.status(404).json({error}));
 }
 
