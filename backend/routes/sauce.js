332f2cffade86d35b6147297ecc0a0fee3d4f749
Mai201 <64534832+Mai201@users.noreply.github.com>
2020-09-08 15:05:08 +0200

    update lisibility

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const auth=require('../middleware/auth');
-const multer=require('../middleware/multer-config');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-const sauceCtrl= require('../controllers/sauce');
+const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
